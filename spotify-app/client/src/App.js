bfd4c15e1b516818b327e3a8da378b297485ab43
au01gh <59246763+au01gh@users.noreply.github.com>
2021-04-17 10:52:40 -0700

    Add files via upload

---
+++ spotify-app/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href='https://localhost:8888/callback'>
+        <button>Login With Spotify</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
