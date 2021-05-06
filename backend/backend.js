e6d3f2c30532d6e17c193cff54a0d7c129a68e62
Alan Nguyen <nguyenalan2000@gmail.com>
2021-05-05 20:34:58 -0700

    lol

--- backend/backend.js
+++ backend/backend.js
@@ -1,30 +1,7 @@
-const express        = require('express')
-const bodyParser     = require('body-parser')
-var firebase         = require('firebase')
-require("firebase/firestore");
 const admin = require('firebase-admin');
-admin.initializeApp();
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 
 const db = admin.firestore();
-
-
-const firebaseConfig = require('./prod.json');
-firebase.initializeApp(firebaseConfig)
-
-const app = express()
-const port = 3000
-
-app.getUsersCollections('/', (req, res) => {
-    console.log("getMyCollections")
-    const docRef = db.collection('excercises').doc('pushups');
-})
-
-app.addCollection('/addCollection', (req, res) => {
-    console.log("adding a collection", req)
-    
-}
-)
-
-app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
-})
