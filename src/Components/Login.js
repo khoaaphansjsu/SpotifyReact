286b7cc5ffcd94d753741e58b45c3d0e14fb70d3
Alan Nguyen <nguyenalan2000@gmail.com>
2021-05-22 23:09:18 -0700

    made login page pretty :D

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
+import '../index.css';
 
 export default class Login extends React.Component {
 
+  
   render() {
+    const div = {
+      backgroundColor: "#0EA348"
+    }
     return (
-      
-      <div className="component-app">
-        {/* <Button onClick={this.handleLogin} >Login</Button> */}
-        <Button style={{left: "50%"}} href="https://localhost:8888/login">Login</Button>
+      <div style={{div}}className="component-app" >
+        <h1 style={{display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#0EA348"
+            }}>Supafy</h1>
+        <p style={{display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#0EA348"
+            }}>Made for those who need a quick and easy way of creating music playlists</p> 
+        <Button style={{display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#0EA348",
+            color: "blue"
+            }} href="https://localhost:8888/login">Login</Button>
       </div>
     );
   }
