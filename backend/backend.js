fba690778e416d9e7f49359c82275a7c3a6e7873
Alan Nguyen <nguyenalan2000@gmail.com>
2021-05-22 23:18:40 -0700

    dsfs

--- backend/backend.js
+++ backend/backend.js
@@ -191,37 +191,17 @@ app.get('/getMyCollections', async (req, res) => {
   // res.json(data);
 })
 
-//add thing to collection
-app.get('/addCollection', async (req, res) => {
-  const userId = req.query.userId;
-  const collection  = req.query.collectionName;
-  const addTo = req.query.thingToAdd
-  db.collection('users').doc(userId).collection('super').doc(collection).update(
-  {links:
-          firebase.firestore.FieldValue.arrayUnion(addTo)
-  })
+app.get('/addCollection', (req, res) => {
   console.log("adding a collection", req)
 })
 
-//remove thing from collection
-app.get('/removeCollection', async (req, res) => {
+function add(email, collectionName, thingToAdd) {
   const userId = req.query.userId;
-  const collection  = req.query.collectionName;
-  const addTo = req.query.thingToAdd
-  db.collection('users').doc(userId).collection('super').doc(collection).update(
-    {links:
-        firebase.firestore.FieldValue.arrayRemove(thingToRemove)
-    })
-})
-
-app.get('/createEmpty', async (req, res) => {
-  const userId = req.query.userEmail;
-  const links = {
-    link: []
-  }
-  console.log("Create empty collection in " + userId)
-  db.collection('users').doc(userId).collection('super').doc("somedefaultname").set(links)
-})
+  db.collection('users').doc(email).collection('super').doc(collectionName).update(
+  {links:
+          firebase.firestore.FieldValue.arrayUnion(thingToAdd)
+  })
+}
 
 app.get("/getCollection", async (req, res) => {
   console.log("getting collection " + req.query.uuid)
